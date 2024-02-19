a951a60099e41fd86e2dcdfc8cb4ee53d0776c51
Facundo Urteaga <facundonahuelurteaga@gmail.com>
2024-02-19 18:46:51 -0300

    prueba

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def developer(developer: str):
 @app.get('/userdata')
 def userdata(user_id: str):
     try:
-        resultado = funciones.userdata(user_id)
+        resultado = userdata2(user_id)
         return resultado
     
     except Exception as e:
@@ -54,3 +54,46 @@ def suma_sencilla(primer_valor : int, segundo_valor : int):
     lista = [primer_valor, segundo_valor]
     suma = np.sum(lista)
     return suma 
+
+########################################################################################################################
+
+def userdata2(user_id : str):
+
+    try:
+        
+        df_games_tec = pd.read_parquet('df_games_tec.parquet')
+        df_games_genres = pd.read_parquet('df_games_genres.parquet')
+        df_games_specs = pd.read_parquet('df_games_specs.parquet')
+        df_games_tags = pd.read_parquet('df_games_tags.parquet')
+        df_reviews_con_sa = pd.read_parquet('df_reviews_con_sa.parquet')
+        df_items = pd.read_parquet('df_items.parquet')
+
+        # Obtener recomendaciones
+        reco_usuario = df_reviews_con_sa[df_reviews_con_sa['user_id'] == user_id]
+        reco_positiva = len(reco_usuario[reco_usuario['recommend'] == True])
+
+        df_items_filtrado = df_items[df_items['user_id'] == user_id]
+
+        df_func_2 = pd.merge(df_games_tec[["item_id","price"]], df_items_filtrado[["user_id","item_id"]], on='item_id', how='inner')
+
+        # Calcula el total del dinero gastado por el usuario
+        dinero_gastado = round(df_func_2['price'].sum(),2)
+
+        # Calcula el porcentaje de recomendación del usuario
+        porcentaje_recomendacion = round((reco_positiva / len(df_func_2)) * 100,2)
+
+        # Calcula la cantidad total de items comprados por el usuario
+        cantidad_items = len(df_func_2)
+
+        # Retorna los resultados en un diccionario con el formato deseado
+        resultado = {
+            "Usuario": user_id,
+            "Dinero gastado": f"{dinero_gastado} USD",
+            "% de recomendación": f"{porcentaje_recomendacion:.2f}%",
+            "Cantidad de items": cantidad_items }
+        
+        return resultado
+
+    except Exception as e:
+        
+        return {"Error": str(e)}
