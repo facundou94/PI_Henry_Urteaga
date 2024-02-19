a3f2271625006cb2fd75138cb60eb9f83e4aef18
Facundo Urteaga <facundonahuelurteaga@gmail.com>
2024-02-19 18:10:09 -0300

    pruebarda

--- funciones.py
+++ funciones.py
@@ -73,90 +73,96 @@ def developer(desarrollador : str):
 
 def userdata(user_id : str):
 
-    CargarArchivos()
+    try:
+        
+        CargarArchivos()
 
-    # Obtener recomendaciones
-    reco_usuario = df_reviews_con_sa[df_reviews_con_sa['user_id'] == user_id]
-    reco_positiva = len(reco_usuario[reco_usuario['recommend'] == True])
+        # Obtener recomendaciones
+        reco_usuario = df_reviews_con_sa[df_reviews_con_sa['user_id'] == user_id]
+        reco_positiva = len(reco_usuario[reco_usuario['recommend'] == True])
 
-    df_items_filtrado = df_items[df_items['user_id'] == user_id]
+        df_items_filtrado = df_items[df_items['user_id'] == user_id]
 
-    df_func_2 = pd.merge(df_games_tec[["item_id","price"]], df_items_filtrado[["user_id","item_id"]], on='item_id', how='inner')
+        df_func_2 = pd.merge(df_games_tec[["item_id","price"]], df_items_filtrado[["user_id","item_id"]], on='item_id', how='inner')
 
-    # Calcula el total del dinero gastado por el usuario
-    dinero_gastado = round(df_func_2['price'].sum(),2)
+        # Calcula el total del dinero gastado por el usuario
+        dinero_gastado = round(df_func_2['price'].sum(),2)
 
-    # Calcula el porcentaje de recomendación del usuario
-    porcentaje_recomendacion = round((reco_positiva / len(df_func_2)) * 100,2)
+        # Calcula el porcentaje de recomendación del usuario
+        porcentaje_recomendacion = round((reco_positiva / len(df_func_2)) * 100,2)
 
-    # Calcula la cantidad total de items comprados por el usuario
-    cantidad_items = len(df_func_2)
+        # Calcula la cantidad total de items comprados por el usuario
+        cantidad_items = len(df_func_2)
 
-    # Retorna los resultados en un diccionario con el formato deseado
-    resultado = {
-        "Usuario": user_id,
-        "Dinero gastado": f"{dinero_gastado} USD",
-        "% de recomendación": f"{porcentaje_recomendacion:.2f}%",
-        "Cantidad de items": cantidad_items }
+        # Retorna los resultados en un diccionario con el formato deseado
+        resultado = {
+            "Usuario": user_id,
+            "Dinero gastado": f"{dinero_gastado} USD",
+            "% de recomendación": f"{porcentaje_recomendacion:.2f}%",
+            "Cantidad de items": cantidad_items }
+        
+        return resultado
+
+    except Exception as e:
+        
+        return {"Error": str(e)}
+
+    #######################################################################################################################################
+
+    def UserForGenre(genero):
+
+        CargarArchivos()
+
+        # Filtro inicialmente los juegos del genero seleccionado
+        df_genre = df_games_genres[df_games_genres[genre] == True]
+        # Filtro solo jugadores que hayan jugado juegos de este género
+        df_func_3_aux = pd.merge(df_items[["user_id","item_id",'playtime_forever']], df_genre[["item_id"]], on='item_id', how='inner')
+        df_func_3 = pd.merge(df_func_3_aux, df_games_tec[["item_id",'release_year']], on='item_id', how='inner')
+
+        # Calcula la suma total de playtime_forever para cada usuario
+        suma_playtime_por_usuario = df_func_3.groupby('user_id')['playtime_forever'].sum()
+
+        # Encuentra el usuario con la mayor suma de playtime_forever
+        usuario_max_playtime = suma_playtime_por_usuario.idxmax()
+
+        # Filtra el DataFrame original para obtener solo las filas asociadas con ese usuario
+        df_usuario_max_playtime = df_func_3[df_func_3['user_id'] == usuario_max_playtime]
+
+        # Agrupa las horas jugadas por año para ese usuario
+        horas_por_anio = df_usuario_max_playtime.groupby('release_year')['playtime_forever'].sum()
+
+        # Construye el diccionario con el formato deseado
+        resultado = {
+            "Usuario con más horas jugadas para Género X": usuario_max_playtime,
+            "Horas jugadas": [{"Año": año, "Horas": horas} for año, horas in horas_por_anio.items()]
+                    }
+        
+        return resultado
+
+    #######################################################################################################################################
+
+    def best_developer_year(year : int):
+
+        CargarArchivos()
+
+        df_games_year = df_games_tec[df_games_tec["release_year"] == year]
+
+        df_func_4 = pd.merge(df_games_year[["app_name",'item_id',"developer"]], df_reviews_con_sa[["item_id",'recommend','sentiment_analysis']], on='item_id', how='inner')
+
+        # Calcular el valor de (recommend + sentiment_analysis * 0.25) para cada item_id para cada user_id
+        df_func_4['score'] = df_func_4['recommend'] + df_func_4['sentiment_analysis'] * 0.25
+
+        # Calcular el valor de (recommend + sentiment_analysis * 0.25) para cada desarrollador
+        df_func_4['score'] = df_func_4['recommend'] + df_func_4['sentiment_analysis'] * 0.25
+
+        # Agrupar por desarrolladores y obtener el top 3
+        top_3_desarrolladores = df_func_4.groupby('developer')['score'].sum().nlargest(3)
+
+        # Construir una lista de diccionarios con el formato deseado
+        resultado = [{"Puesto {}: {}".format(i + 1, desarrollador[0]): desarrollador[1]} for i, desarrollador in enumerate(top_3_desarrolladores.items())]
+
+        return resultado
     
-    return resultado
-
-#######################################################################################################################################
-
-def UserForGenre(genero):
-
-    CargarArchivos()
-
-    # Filtro inicialmente los juegos del genero seleccionado
-    df_genre = df_games_genres[df_games_genres[genre] == True]
-    # Filtro solo jugadores que hayan jugado juegos de este género
-    df_func_3_aux = pd.merge(df_items[["user_id","item_id",'playtime_forever']], df_genre[["item_id"]], on='item_id', how='inner')
-    df_func_3 = pd.merge(df_func_3_aux, df_games_tec[["item_id",'release_year']], on='item_id', how='inner')
-
-    # Calcula la suma total de playtime_forever para cada usuario
-    suma_playtime_por_usuario = df_func_3.groupby('user_id')['playtime_forever'].sum()
-
-    # Encuentra el usuario con la mayor suma de playtime_forever
-    usuario_max_playtime = suma_playtime_por_usuario.idxmax()
-
-    # Filtra el DataFrame original para obtener solo las filas asociadas con ese usuario
-    df_usuario_max_playtime = df_func_3[df_func_3['user_id'] == usuario_max_playtime]
-
-    # Agrupa las horas jugadas por año para ese usuario
-    horas_por_anio = df_usuario_max_playtime.groupby('release_year')['playtime_forever'].sum()
-
-    # Construye el diccionario con el formato deseado
-    resultado = {
-        "Usuario con más horas jugadas para Género X": usuario_max_playtime,
-        "Horas jugadas": [{"Año": año, "Horas": horas} for año, horas in horas_por_anio.items()]
-                }
-    
-    return resultado
-
-#######################################################################################################################################
-
-def best_developer_year(year : int):
-
-    CargarArchivos()
-
-    df_games_year = df_games_tec[df_games_tec["release_year"] == year]
-
-    df_func_4 = pd.merge(df_games_year[["app_name",'item_id',"developer"]], df_reviews_con_sa[["item_id",'recommend','sentiment_analysis']], on='item_id', how='inner')
-
-    # Calcular el valor de (recommend + sentiment_analysis * 0.25) para cada item_id para cada user_id
-    df_func_4['score'] = df_func_4['recommend'] + df_func_4['sentiment_analysis'] * 0.25
-
-    # Calcular el valor de (recommend + sentiment_analysis * 0.25) para cada desarrollador
-    df_func_4['score'] = df_func_4['recommend'] + df_func_4['sentiment_analysis'] * 0.25
-
-    # Agrupar por desarrolladores y obtener el top 3
-    top_3_desarrolladores = df_func_4.groupby('developer')['score'].sum().nlargest(3)
-
-    # Construir una lista de diccionarios con el formato deseado
-    resultado = [{"Puesto {}: {}".format(i + 1, desarrollador[0]): desarrollador[1]} for i, desarrollador in enumerate(top_3_desarrolladores.items())]
-
-    return resultado
-
 #######################################################################################################################################
 
 def developer_reviews_analysis(desarrolladora : str):
